337cc315a3e04054a45bfecb81c41897e271037b
Nora Morsi <morsin@uw.edu>
2021-05-12 11:50:50 -0700

    separation of app and grocery list into two different components

--- CSE481H1Project/App.js
+++ CSE481H1Project/App.js
@@ -1,54 +1,10 @@
-// https://www.youtube.com/watch?v=uLHFPt9B2Os
-
-import React, { useState } from 'react';
-import { StyleSheet, Text, ScrollView, FlatList, View, TextInput } from 'react-native';
-import AddItem from './components/addItem';
-import Header from './components/header';
-import Item from './components/item';
+import React from 'react';
+import GroceryList from './components/groceryList';
 
 const App = () => {
-    const [list, setList] = useState([
-        {text: 'coffee', key: 1},
-        {text: 'bread', key: 2},
-        {text: 'milk', key: 3}
-    ]);
-
-    const styles = StyleSheet.create({
-        container: {
-        },
-        content: {
-          padding: 40,
-        },
-        list: {
-        }
-    });
-
-    const submitHandler = (itemName) => {
-        setList((prevList) => {
-            return [
-                {text: itemName, key: Math.random().toString()},
-                ...prevList];
-        })
-    };
-
-    const deleteHandler = (itemKey) => {
-        setList((prevList) => {
-            return prevList.filter(current => current.key != itemKey);
-        });
-    }
-
-    return (
-        <View style={styles.container}>
-            <Header headerTitle="My Grocery List" />
-            <View style={styles.content}>
-                <AddItem submitHandler={ submitHandler } />
-                <FlatList style={styles.list}
-                    data={list}
-                    renderItem={({item}) => (
-                    <Item item={item} deleteHandler={ deleteHandler }/>)}/>
-            </View>
-        </View>
-    );
+  return (
+    <GroceryList />
+  );
 };
 
 export default App;
---
+++ CSE481H1Project/components/groceryList.js
@@ -0,0 +1,54 @@
+// https://www.youtube.com/watch?v=uLHFPt9B2Os
+
+import React, { useState } from 'react';
+import { StyleSheet, Text, ScrollView, FlatList, View, TextInput } from 'react-native';
+import AddItem from './addItem';
+import Header from './header';
+import Item from './item';
+
+const GroceryList = () => {
+    const [list, setList] = useState([
+        {text: 'coffee', key: 1},
+        {text: 'bread', key: 2},
+        {text: 'milk', key: 3}
+    ]);
+
+    const styles = StyleSheet.create({
+        container: {
+        },
+        content: {
+          padding: 40,
+        },
+        list: {
+        }
+    });
+
+    const submitHandler = (itemName) => {
+        setList((prevList) => {
+            return [
+                {text: itemName, key: Math.random().toString()},
+                ...prevList];
+        })
+    };
+
+    const deleteHandler = (itemKey) => {
+        setList((prevList) => {
+            return prevList.filter(current => current.key != itemKey);
+        });
+    }
+
+    return (
+        <View style={styles.container}>
+            <Header headerTitle="My Grocery List" />
+            <View style={styles.content}>
+                <AddItem submitHandler={ submitHandler } />
+                <FlatList style={styles.list}
+                    data={list}
+                    renderItem={({item}) => (
+                    <Item item={item} deleteHandler={ deleteHandler }/>)}/>
+            </View>
+        </View>
+    );
+};
+
+export default GroceryList;
